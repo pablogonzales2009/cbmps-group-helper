f7067178c2c1c90d0b0b5d920bf85348f29f9ecb
tekkinz <tekkinznoob@gmail.com>
2022-05-02 01:06:51 +0200

    tried some more

--- src/app.js
+++ src/app.js
@@ -2,8 +2,25 @@ const { Client, Intents } = require('discord.js');
 const { token } = require('../config.json');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+
 client.once('ready', () => {
 	console.log('teks group helper is running');
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	// put your command functions here
+	const commands = {
+		'ping': () => (interaction.reply('pong.s')),
+
+
+	};
+
+	const { commandName } = interaction;
+	await commands[commandName];
+
+
 });
 
 
--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -2,16 +2,16 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, token } = require('../config.json');
-
+// this shit dont work wtf
 const commands = [
-	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+	new SlashCommandBuilder().setName('user').setDescription('sets the user thing'),
+	new SlashCommandBuilder().setName('ping2').setDescription('Replies with pong!'),
+	new SlashCommandBuilder().setName('ping3').setDescription('Replies with pong!'),
 ]
 	.map(command => command.toJSON());
 
 const rest = new REST({ version: '9' }).setToken(token);
 
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
+	.then(() => console.log('created commands', commands))
 	.catch(console.error);
